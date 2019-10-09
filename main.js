b6e98941f4f415575a2a66138c6dcb3679c6d8af
Danyale <danyalecw@gmail.com>
2019-10-08 21:45:50 -0400

    update portu page

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+// -----------NAVIGATION TEMPLATE---------------
+class MyTemplate extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML =
+      `<div class="nav">
+        <button onclick="portugal()" title="
+        Portugal">Home</button>
+        <button onclick="lisboa()" title="Lisboa">Lisboa</button>
+        <button onclick="portu()" title="Portu">Portu</button>
+        <button onclick="coimbra()" title="Coimbra">Coimbra</button>
+        <button onclick="sintra()" title="Sintra">Sintra</button>
+        <button onclick="douro()" title="Douro">Douro River Valley</button>
+        <button onclick="alegrete()" title="Alegrete">Alegrete</button>
+      </div>`;
+  }
+}
+
+customElements.define('nav-template', MyTemplate);
 
 // -----------SLIDESHOW LISBOA PAGE---------------
 
-var slideIndex = [1,1];
+var slideIndex = [1, 1];
 var slideId = ["slides1", "slides2"]
-showSlides(1,0);
-showSlides(1,1);
+showSlides(1, 0);
+showSlides(1, 1);
+
 function slides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
@@ -17,33 +36,34 @@ function showSlides(n, no) {
   for (i = 0; i < show.length; i++) {
     show[i].style.display = "none";
   }
-  show[slideIndex[no]-1].style.display = "block";
+  show[slideIndex[no] - 1].style.display = "block";
 }
 // -----------NAVIGATION BUTTONS---------------
 
 function portugal() {
-    window.location.href = "Portugal.html"
+  window.location.href = "Portugal.html"
 }
+
 function lisboa() {
-    window.location.href = "Lisboa.html"
+  window.location.href = "Lisboa.html"
 }
 
 function portu() {
-    window.location.href = "Portu.html"
+  window.location.href = "Portu.html"
 }
 
 function coimbra() {
-    window.location.href = "Coimbra.html"
+  window.location.href = "Coimbra.html"
 }
 
 function sintra() {
-    window.location.href = "Sintra.html"
+  window.location.href = "Sintra.html"
 }
 
 function douro() {
-    window.location.href = "Douro.html"
+  window.location.href = "Douro.html"
 }
 
 function alegrete() {
-    window.location.href = "Alegrete.html"
+  window.location.href = "Alegrete.html"
 }
